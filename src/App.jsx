e4bf570ec99dc2cf4d277ab7d71bfaca1f848250
Himanshu12866 <manshu010m@gmail.com>
2025-10-12 20:16:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+  <>
+    <div>
+      <h1 className='text-8xl text-emerald-600'>Jai Shri Ram 
+      
+      
+      </h1>
+    </div>
+  </>
+  )
+}
+
+export default App
